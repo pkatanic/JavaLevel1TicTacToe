b6f5d4f65b6ebb350d63ab25d0c755c65a20e8ae
pkatanic <p.katanic@yahoo.com>
2020-11-09 18:22:08 +0300

    Homework added to original code

--- 3/src/TicTacToe.java
+++ 3/src/TicTacToe.java
@@ -17,11 +17,14 @@ public class TicTacToe {
     private static int fieldSizeY;
     // field
     private static char[][] field;
+    public static int evenSymbols; //число  одинаковых значений подряд для победы
+
 
     // initField
     private static void initField() {
-        fieldSizeX = 3;
-        fieldSizeY = 3;
+        fieldSizeX = 5;
+        fieldSizeY = 5;
+        evenSymbols=4;
         field = new char[fieldSizeY][fieldSizeX];
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
@@ -52,7 +55,7 @@ public class TicTacToe {
         int x;
         int y;
         do {
-            System.out.print("Р’РІРµРґРёС‚Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ (РіРѕСЂ Рё РІРµСЂ) РѕС‚ 1 РґРѕ " + fieldSizeX + " С‡РµСЂРµР· РїСЂРѕР±РµР»>> ");
+            System.out.print("Введите координаты (X и Y) от 1 до " + fieldSizeX + " через пробел");
             x = SCANNER.nextInt() - 1;
             y = SCANNER.nextInt() - 1;
         } while (!isCellValid(x, y) || !isCellEmpty(x, y));
@@ -76,9 +79,35 @@ public class TicTacToe {
     private static boolean isCellValid(int x, int y) {
         return x >= 0 && y >= 0 && x < fieldSizeX && y < fieldSizeY;
     }
+    //Проверка диагонали
+    static boolean checkDiagonal(char c, int X, int Y) {
+        boolean right = true;
+        boolean left =true;
+
+        for(int i =0; i<evenSymbols; i++) {
+            right = right &(field[i + X][i+Y] == c);
+            left = left & (field[evenSymbols-i-1 + X][i + Y] == c);
+        }
+        if(right||left) return true;
+        return false;
+    }
+    // проверка горизонтальных и вертикальных линий
+   static boolean checkLines(char c, int X, int Y){
+        boolean cols = true;
+        boolean rows = true;
+        for(int col =X; col<evenSymbols+X; col++){
+            for(int row = Y; row<evenSymbols+Y; row++) {
+                cols = cols & (field[col][row] == c);
+                rows = rows & (field[row][col] == c);
+            }
+            if(cols||rows)
+                return true;
+        }
+        return false;
+    }
     // checkWin
     private static boolean checkWin(char c) {
-        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+       /* if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
         if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
         if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
 
@@ -87,10 +116,18 @@ public class TicTacToe {
         if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
 
         if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
-        if (field[2][0] == c && field[1][1] == c && field[0][2] == c) return true;
+        if (field[2][0] == c && field[1][1] == c && field[0][2] == c) return true; */
 
+       for(int col = 0; col<fieldSizeX-evenSymbols+1;col++){
+           for(int row =0; row<fieldSizeY-evenSymbols+1; row++){
+
+              if(checkDiagonal(c,col,row) || checkLines(c,col,row)) return true;
+           }
+       }
         return false;
     }
+
+
     // checkDraw
     private static boolean isDraw() {
         for (int y = 0; y < fieldSizeY; y++) {
